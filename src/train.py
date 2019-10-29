b6d3887d3ae3c6b568ecd95d4d11eb647ca59f6f
amr-amr <andrei.romascanu@mail.mcgill.ca>
2019-10-29 18:45:02 -0400

    fix: updated train

--- src/train.py
+++ src/train.py
@@ -5,10 +5,11 @@ import torch
 from torch import nn, optim
 from torchvision.transforms import transforms
 from src.data import ModifiedMNISTDataset, train_test_split
+from src.model import SimpleCNN, pretrained_resnet18
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 import pandas as pd
-import numpy as np
+
 
 def eval_model(model, criterion, dataloader, device):
     model.eval()
@@ -85,36 +86,42 @@ def train_model(model, criterion, optimizer, scheduler,
 
 
 if __name__ == '__main__':
-    import torchvision.models as models
-    device = torch.device("cpu")
-    model_ft = models.resnet18(pretrained=True)
-    num_ftrs = model_ft.fc.in_features
-    model_ft.fc = nn.Linear(num_ftrs, 10)
-    criterion = nn.CrossEntropyLoss()
-    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-    exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    DEVICE = torch.device("cpu")
+    # MODEL = SimpleCNN(256, 10)
+    # TO_RGB = False
+    MODEL = pretrained_resnet18(10)
+    TO_RGB = True
+    CRITERION = nn.CrossEntropyLoss()
+    # OPTIMIZER = optim.SGD(MODEL.parameters(), lr=0.001, momentum=0.9)
+    OPTIMIZER = optim.Adam(MODEL.parameters())
+    LR_SCHEDULER = optim.lr_scheduler.StepLR(OPTIMIZER, step_size=7, gamma=0.1)
+    BATCH_SIZE = 4
+
 
     t = transforms.Compose([
             transforms.Resize(256),
-            transforms.CenterCrop(224),
             transforms.ToTensor()])
 
     # using entire dataset (6hrs/epoch on my laptop)
-    # x = pd.read_pickle("../data/train_max_x")
-    # y = pd.read_csv("../data/train_max_y.csv")
-    #
-    # train_x, train_y, valid_x, valid_y = train_test_split(x, y)
-    #
-    # train_dataset = ModifiedMNISTDataset(train_x, train_y,
-    #                                      to_rgb=True, transform=t)
-    # valid_dataset = ModifiedMNISTDataset(valid_x, valid_y,
-    #                                      to_rgb=True, transform=t)
+    x = pd.read_pickle("../data/train_max_x")
+    y = pd.read_csv("../data/train_max_y.csv")
 
-    train_dataset = ModifiedMNISTDataset.from_files("../data/debug_train_x", "../data/debug_train_y.csv",
-                                                    to_rgb=True, transform=t)
-    valid_dataset = ModifiedMNISTDataset.from_files("../data/debug_valid_x", "../data/debug_valid_y.csv",
-                                                    to_rgb=True, transform=t)
-    train_dataloader = DataLoader(train_dataset, batch_size=10, shuffle=True)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=10, shuffle=True)
+    train_x, train_y, valid_x, valid_y = train_test_split(x, y)
 
-    train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, train_dataloader, valid_dataloader, device)
+    train_dataset = ModifiedMNISTDataset(train_x, train_y,
+                                         to_rgb=TO_RGB, transform=t)
+    valid_dataset = ModifiedMNISTDataset(valid_x, valid_y,
+                                         to_rgb=TO_RGB, transform=t)
+
+    # train_dataset = ModifiedMNISTDataset.from_files("../data/debug_train_x",
+    #                                                 "../data/debug_train_y.csv",
+    #                                                 to_rgb=False, transform=t)
+    # valid_dataset = ModifiedMNISTDataset.from_files("../data/debug_valid_x",
+    #                                                 "../data/debug_valid_y.csv",
+    #                                                 to_rgb=False, transform=t)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    train_model(MODEL, CRITERION, OPTIMIZER, LR_SCHEDULER, train_dataloader, valid_dataloader, DEVICE)
